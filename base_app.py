471d35908b025d6d46a36cee2a8333c4df0fd969
Nomvuselelo <65552536+Nomvuselelo@users.noreply.github.com>
2021-06-21 17:14:41 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -25,11 +25,147 @@
 import streamlit as st
 import joblib,os
 
-# Data dependencies
+# Standard libraries
+import re
+import csv
+import nltk
+import spacy
+import string
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
+
+import matplotlib.style as style 
+sns.set(font_scale=1.5)
+style.use('seaborn-pastel')
+style.use('seaborn-poster')
+from PIL import Image
+from wordcloud import WordCloud
+
+# Downloads
+#nlp = spacy.load('en')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+# Preprocessing
+import en_core_web_sm
+from collections import Counter
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import stopwords, wordnet  
+from sklearn.feature_extraction.text import CountVectorizer   
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer 
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+
+# Building classification models
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+# Model evaluation
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix, f1_score, precision_score, recall_score
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Cleaning tweets
+
+def TweetCleaner(tweet):
+    
+    """This function uses regular expressions to remove url's, mentions, hashtags, 
+       punctuation, numbers and any extra white space from tweets after converting 
+       everything to lowercase letters.
+    """
+    tweet = re.sub(r'^RT ','', re.sub(r'https://t.co/\w+', '', tweet).strip())         # Remove url's
+    tweet = tweet.lower()                                    # Convert everything to lowercase 
+    tweet = re.sub('@[\w]*','',tweet)                        # Remove mentions  
+    tweet = re.sub(r'#\w*', '', tweet)                       # Remove hashtags
+    tweet = re.sub(r'\d+', '', tweet)                        # Remove numbers
+    tweet = re.sub(r"[,.;':@#?!\&/$]+\ *", ' ', tweet)       # Remove punctuation
+    tweet = re.sub(r"U+FFFD ", ' ', tweet)                   # Remove that funny diamond
+    tweet = re.sub(r'\s\s+', ' ', tweet)                     # Remove extra whitespace
+    tweet = tweet.lstrip(' ')                                # Remove space in front of tweet  
+       
+    emojis = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # removes emoticons,
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+
+    tweet = emojis.sub(r'', tweet)
+
+    return tweet
+
+# Clean the tweets in the message column
+df['message'] = df['message'].apply(TweetCleaner)
+
+
+#Lets create an analysis column
+
+def getAnalysis(sentiment):
+    if sentiment == 0:
+        return 'Neutral'
+    elif sentiment == 1:
+        return 'Pro'
+    elif sentiment == -1:
+        return 'Anti'
+    else: 
+        return 'News'
+df['analysis'] = df['sentiment'].apply(getAnalysis)
+
+#Lematisation
+
+def lemma(df):
+    
+    """
+    This function modifies the original train dataframe.
+    A new column for the length of each tweet is added.
+    The tweets are then tokenized and each word is assigned a part of speech tag 
+    before being lemmatized
+    """
+    df['length'] = df['message'].str.len()
+    df['tokenized'] = df['message'].apply(word_tokenize)
+    df['pos_tags'] = df['tokenized'].apply(nltk.tag.pos_tag)
+
+    def get_wordnet_pos(tag):
+
+        if tag.startswith('J'):
+            return wordnet.ADJ
+
+        elif tag.startswith('V'):
+            return wordnet.VERB
+
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+
+        elif tag.startswith('R'):
+            return wordnet.ADV
+    
+        else:
+            return wordnet.NOUN
+        
+    wnl = WordNetLemmatizer()
+    df['pos_tags'] = df['pos_tags'].apply(lambda x: [(word, get_wordnet_pos(pos_tag)) for (word, pos_tag) in x])
+    df['lemmatized'] = df['pos_tags'].apply(lambda x: [wnl.lemmatize(word, tag) for word, tag in x])
+    df['lemmatized'] = [' '.join(map(str, l)) for l in df['lemmatized']]  
+    return df
+
 
 # Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
+news_vectorizer = open("resources/lsvc_op.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
